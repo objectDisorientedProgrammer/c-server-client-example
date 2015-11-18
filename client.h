ef3481695701d99b00babcef621feac48a6c2251
Douglas Chidester <tikiman92@gmail.com>
2015-11-17 22:21:56 -0800

    header file for client

---
+++ client.h
@@ -0,0 +1,20 @@
+/*
+	client.h
+	Author: Douglas Chidester
+	Date:	17 November 2015
+*/
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+#define PORT 71153
+#define SIZE 32768
+
+#endif
